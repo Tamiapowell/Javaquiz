8d91b45d63334731b5f5ccd9d581d6eba2dcc47b
Tamia Powell <tamiapowell@Tamias-MacBook-Pro-2.local>
2020-09-02 15:09:20 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int classLoad = 5;
+        float gpa = 3.80f;
+        boolean juniorLevel = true;
+        String name = ("Tamia.");
+        System.out.println("My name is " + name );
+        System.out.println("This year I have " + classLoad + " classes.");
+        System.out.println("My GPA is a " + gpa + " this semester.");
+        System.out.println("It is " + juniorLevel + " that I am a junior in college.");
+
+
+
+    }
+}
